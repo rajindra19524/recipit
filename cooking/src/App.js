887ec957644164dc252e067974ff13d18275c095
Rajindra Pitigalaarachchi <95735946+rajindra19524@users.noreply.github.com>
2024-12-02 10:35:42 +0530

    created pages and routes

---
+++ cooking/src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import './App.css';
+import Home from './pages/Home'
+import Create from './pages/Create';
+import Search from './pages/Search';
+import Recipe from './pages/Recipe';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route/>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
