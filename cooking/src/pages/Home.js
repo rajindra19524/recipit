ea9ce5e7e715d77145e3aa6a621a0995f09e433c
Rajindra Pitigalaarachchi <95735946+rajindra19524@users.noreply.github.com>
2024-12-02 17:19:52 +0530

    creating RecipeList component

--- cooking/src/pages/Home.js
+++ cooking/src/pages/Home.js
@@ -1,5 +1,6 @@
 import './Home.css'
 import { useFetch } from '../hooks/useFetch';
+import RecipeList from '../components/RecipeList';
 
 function Home() {
      const {data,isPending,error} = useFetch('http://localhost:3000/recipes')
@@ -7,9 +8,7 @@ function Home() {
         <div className='home'>
             {error && <p className="error">{error}</p>}
             {isPending && <p className="loading">is Loading...</p>}
-            {data && data.map(recipe => (
-                <h2 key={recipe.id}>{recipe.title}</h2>
-            ))}
+            {data && <RecipeList recipes={data} />}
         </div>
     )
 }
