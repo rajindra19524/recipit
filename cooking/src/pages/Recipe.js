b5e511428aeb17775bd4f83f162063afc7304abf
Rajindra Pitigalaarachchi <95735946+rajindra19524@users.noreply.github.com>
2024-12-03 09:12:57 +0530

    Recipe details template

--- cooking/src/pages/Recipe.js
+++ cooking/src/pages/Recipe.js
@@ -10,10 +10,19 @@ export default function Recipe() {
     const { data: recipe, error, isPending } = useFetch(url)
     
     return (
-        <div>
+        <div className="recipe">
             {error && <p className='error'>{error}</p>}
             {isPending && <p className='loading'>Loading...</p>}
-            {recipe && <h1>{recipe.title}</h1>}
+            {recipe && (
+                <>
+                    <h2 className='page-title'>{recipe.title}</h2>
+                    <p>Takes {recipe.cookingTime} to cook.</p>
+                    <ul>
+                        {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+                    </ul>
+                    <p>{recipe.method}</p>
+                </>
+            )}
         </div>
     )
 }
