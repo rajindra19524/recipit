699c2c5ea1a60047f4ebfef7c90ee2dc2bc24011
Rajindra Pitigalaarachchi <95735946+rajindra19524@users.noreply.github.com>
2024-12-04 09:43:21 +0530

    make post request

--- cooking/src/pages/Create.js
+++ cooking/src/pages/Create.js
@@ -10,7 +10,7 @@ export default function Create() {
     const [ingredients, setIngredients] = useState([])
     const ingredientInput = useRef(null)
 
-    const [postData, data, error] = useFetch('http://localhost:3000/recipes','POST')
+    const {postData, data, error} = useFetch('http://localhost:3000/recipes','POST')
     
     const handleAdd = (e) => {
         e.preventDefault()
